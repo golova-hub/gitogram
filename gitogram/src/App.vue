0b97a6c42422c88e8c0e29f3b656a00802d2ef6c
Golova <mi.golova@gmail.com>
2022-03-28 11:40:43 +0300

    test

---
+++ gitogram/src/App.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <feeds/>
+</template>
+
+<script>
+import { feeds } from "./pages/feeds";
+export default {
+  name: "App",
+  components: {
+    feeds,
+  },
+};
+</script>
+
+<style lang="scss" src="./global.scss"></style>
---
+++ gitogram/src/pages/feeds/feeds.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="topline">
+    <topline>
+      <template #headline>
+        <div class="icon">
+          <icon name="home" />
+        </div>
+      </template>
+      <template #content>
+        <h1>content</h1>
+      </template>
+    </topline>
+  </div>
+</template>
+<script>
+import { topline } from '../../components/topline';
+import { icon } from '../../icons';
+
+export default {
+  name: "feeds",
+  components: {
+    topline,
+    icon
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .icon {
+    color: red;
+    width: 50px;
+  }
+</style>
+
